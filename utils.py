c0c4dff99bf4de150bd52578d29cd33ac6facb25
Alyssa Lee <isster@icloud.com>
2026-04-10 03:47:46 -0400

    reorganize train test code and take dimensions as input to nn init

--- utils.py
+++ utils.py
@@ -83,18 +83,51 @@ class ContextLinearRegression:
 
 
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, dim_in, dim_out):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(3, 20),
+            nn.Linear(dim_in, 20),
             nn.ReLU(),
             nn.Linear(20, 20),
             nn.ReLU(),
-            nn.Linear(20, 1)
+            nn.Linear(20, dim_out)
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+
+
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device):
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y)
+    test_loss /= num_batches
+    print(f"Test Error: \n Avg loss: {test_loss:>8f} \n")
